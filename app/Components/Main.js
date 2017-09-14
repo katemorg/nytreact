6bea526fcb66fb66e01e4bf5ef86c36e89b7225b
Kate Morgenstern <katemorg51@gmail.com>
2017-09-14 12:41:41 -0400

    working, not deployed

---
+++ app/Components/Main.js
@@ -0,0 +1,119 @@
+var axios = require('axios');
+var React = require('react');
+
+var Form = require('./Children/Form');
+var Results = require('./Children/Results');
+var Saved = require('./Children/Saved');
+
+var helpers = require('./utils/helpers.js');
+
+
+var Main = React.createClass({
+
+	getInitialState: function(){
+		return {
+			topic: "",
+			startYear: "",
+			endYear: "",
+			results: [],
+			savedArticles: []
+		}
+	},	
+
+	setTerm: function(tpc, stYr, endYr){
+		this.setState({
+			topic: tpc,
+			startYear: stYr,
+			endYear: endYr
+		})
+	},
+
+	saveArticle: function(title, date, url){
+		helpers.postArticle(title, date, url);
+		this.getArticle();
+	},
+
+	deleteArticle: function(article){
+		console.log(article);
+		axios.delete('/api/saved/' + article._id)
+			.then(function(response){
+				this.setState({
+					savedArticles: response.data
+				});
+				return response;
+			}.bind(this));
+
+		this.getArticle();
+	},
+
+	getArticle: function(){
+		axios.get('/api/saved')
+			.then(function(response){
+				this.setState({
+					savedArticles: response.data
+				});
+			}.bind(this));
+	},
+
+	componentDidUpdate: function(prevProps, prevState){
+
+		if(prevState.topic != this.state.topic){
+			console.log("UPDATED");
+
+			helpers.runQuery(this.state.topic, this.state.startYear, this.state.endYear)
+				.then(function(data){
+					console.log(data);
+					if (data != this.state.results)
+					{
+						this.setState({
+							results: data
+						})
+					}
+				}.bind(this))
+		}
+	},
+
+	componentDidMount: function(){
+		axios.get('/api/saved')
+			.then(function(response){
+				this.setState({
+					savedArticles: response.data
+				});
+			}.bind(this));
+	},
+
+	render: function(){
+		return(
+
+			<div className="container">
+
+				<div className="row">
+
+					<div className="jumbotron" style={{'backgroundImage': 'url(./assets/images/newspaper.jpg)', 'backgroundRepeat': 'no-repeat', 'backgroundPosition': 'center', 'backgroundSize': '100% 100%', 'backgroundAttachment': 'fixed'}}>
+						<h2 className="text-center" style={{'color': 'white', 'textShadow': '3px 3px 10px black', 'fontSize': '54px'}}>New York Times Article Search and Save</h2>
+						<p className="text-center" style={{'color': 'white', 'textShadow': '3px 3px 10px black', 'fontSize': '24px'}}>Search for and save articles of interest!</p>
+					</div>
+				</div>
+				<div className="row">
+
+					<Form setTerm={this.setTerm}/>
+
+				</div>
+
+				<div className="row">
+			
+					<Results results={this.state.results} saveArticle={this.saveArticle}/>
+
+				</div>
+
+				<div className="row">
+				
+					<Saved savedArticles={this.state.savedArticles} deleteArticle={this.deleteArticle} />
+
+				</div>
+			</div>
+		)
+	}
+});
+
+module.exports = Main;
